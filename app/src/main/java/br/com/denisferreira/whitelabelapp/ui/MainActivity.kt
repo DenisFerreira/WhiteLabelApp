05a72fecc7a0ad1aebc61c6c7226b0a78715ccbf
DenisFerreira <denis_ferreira@outlook.com>
2021-07-24 15:07:24 -0300

    Firebase Configuration and Products Flavor

---
+++ app/src/main/java/br/com/denisferreira/whitelabelapp/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.denisferreira.whitelabelapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import br.com.denisferreira.whitelabelapp.R
+import br.com.denisferreira.whitelabelapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+}
--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package br.com.douglasmotta.whitelabeltutorial.util
-
-const val COLLECTION_ROOT = "data"
-const val COLLECTION_PRODUCTS = "products"
-const val STORAGE_IMAGES = "images"
-const val PRODUCT_KEY = "product"
