1780121331b68f4f2a06491fe39d676d3f51d44f
DenisFerreira <denis_ferreira@outlook.com>
2021-07-29 18:27:00 -0300

    Finish FirebaseProductDataSource

--- app/src/main/java/br/com/denisferreira/whitelabelapp/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/denisferreira/whitelabelapp/data/FirebaseProductDataSource.kt
@@ -5,8 +5,10 @@ import br.com.denisferreira.whitelabelapp.BuildConfig
 import br.com.denisferreira.whitelabelapp.domain.model.Product
 import br.com.denisferreira.whitelabelapp.util.COLLECTION_PRODUCTS
 import br.com.denisferreira.whitelabelapp.util.COLLECTION_ROOT
+import br.com.denisferreira.whitelabelapp.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -16,7 +18,7 @@ class FirebaseProductDataSource(
 
     private val documentReference =
         firebaseFirestore.document("$COLLECTION_ROOT/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/")
-    private val storeReference = firebaseStorage.reference
+    private val storageReference = firebaseStorage.reference
 
     override suspend fun getProducts(): List<Product> {
         return suspendCoroutine { continuation ->
@@ -38,10 +40,41 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childrenReference = storageReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+            childrenReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapShot ->
+                    taskSnapShot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        val path = uri.toString()
+                        continuation.resumeWith(Result.success(path))
+                    }
+
+                }
+                .addOnFailureListener {
+                    continuation.resumeWith(Result.failure(it))
+                }
+
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            documentReference.collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+                }
+                .addOnFailureListener {
+                    continuation.resumeWith(Result.failure(it))
+                }
+            val randomKey = UUID.randomUUID()
+            val childrenReference = storageReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+        }
     }
 }
